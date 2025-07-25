074e95a30cc0b6b0149ac09c607bcd2b73d92c1d
KSY <sye.g112@gmail.com>
2025-07-25 20:17:17 +0900

    basic nodeJs server

--- 01_basic_server/1_server.js
+++ 01_basic_server/1_server.js
@@ -7,4 +7,15 @@ const port = 3000;
 
 // req -> request -> 요청
 // res -> response -> 응답
-http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end("<h1>Hello World</h1>");
+});
+
+server.listen(port, host, () => {
+  console.log("server running on http://localhost:3000");
+});
+
+// node 1_server.js 명령어로 실행할 수 있다.
+// chorme에서 http://localhost:3000을 하면 이 서버에 요청을 할 수 있다.
+// chorme에서 Hello World가 뜨는 것을 볼 수 있다.
