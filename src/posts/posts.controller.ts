d0a7a66968224e2e20bf4ed7f420797eb90145be
KSY <sye.g112@gmail.com>
2025-07-26 20:06:02 +0900

    posts controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commetnCount: number;
+}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 10000000,
+      commetnCount: 9999999,
+    };
+  }
 }
