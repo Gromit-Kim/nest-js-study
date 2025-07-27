5acf47315c2fe2e349bf6ec25d85089332bfd408
KSY <sye.g112@gmail.com>
2025-07-27 17:46:42 +0900

    type orm study 일반 디렉토리로 인식

---
+++ 02_typeorm/src/app.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+import { Repository } from 'typeorm';
+
+@Controller()
+export class AppController {
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userRepository: Repository<UserModel>,
+  ) {}
+
+  @Post('users')
+  postUser() {
+    return this.userRepository.save({
+      title: 'test title',
+    });
+  }
+
+  @Get('users')
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  @Patch('users/:id')
+  async patchUser(@Param('id') id: string) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return this.userRepository.save({
+      ...user,
+      title: user.title + '0',
+    });
+  }
+}
---
+++ 02_typeorm/src/entity/user.entity.ts
@@ -0,0 +1,57 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserModel {
+  // @PrimaryGeneratedColumn()
+  // Primary Column은 모든 테이블에서 기본적으로 존재해야한다.
+  // 테이블 안에서 각각의 Row를 구분할 수 있는 칼람이다.
+  // @PrimaryColumn()
+  // @PrimaryGeneratedColumn('uuid')
+  // PrimaryGeneatedColumn은 순서대로 값이 올라간다. 1, 2, 3, 4 ....
+  // uuid의 경우는 adbasd12345cdfdf-asdfasdf1234dafdadsf-123asdfbcad 처럼 나온다.
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    // 데이터베이스에서 인지하는 칼럼 타입
+    // 자동으로 추론되어 넣지 않아도 된다.
+    type: 'varchar',
+    // 데이터베이스의 칼럼 이름
+    // 작성하지 않으면 프로퍼티 이름으로 자동 처리됨.
+    name: 'title',
+    // 값의 길이
+    // 입력할 수 있는 글자의 길이가 300
+    length: 300,
+    // null이 가능한지
+    nullable: true,
+    // true면 처음 저장할 때만 값 지정 가능
+    // 이후에는 값 변경 불가능
+    update: false,
+  })
+  title: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  // 데이터가 업데이트될 때마다 1씩 올라간다.
+  // 처음 생성되면 값은 1이다.
+  // save() 함수가 몇 번 불렸는지 기억한다.
+  @VersionColumn()
+  version: number;
+
+  // generated annotation은 column annotation과 함께 사용해야 한다.
+  @Column()
+  @Generated('uuid') // pk는 아니지만 자동으로 1씩 올라가는 게 increment, uuid를 하면 string으로 타입 변경
+  additionalId: string; // number or string
+}
