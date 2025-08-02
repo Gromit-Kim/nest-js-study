25729e55bb4d572cc4a404b7faedd8e46b29ff6e
KSY <sye.g112@gmail.com>
2025-08-02 10:16:44 +0900

    추가: users typerom module import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserModel } from '02_typeorm/src/entity/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserModel])],
   controllers: [UsersController],
   providers: [UsersService],
 })
