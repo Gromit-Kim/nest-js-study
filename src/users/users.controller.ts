c0793114233a63cfd772cc0dad02f1820a50cc7c
KSY <sye.g112@gmail.com>
2025-08-03 22:21:06 +0900

    refactor(user): 사용자 생성 - pick을 이용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,6 @@ export class UsersController {
     @Body('email') email: string,
     @Body('password') password: string,
   ) {
-    return this.usersService.createUser(nickname, email, password);
+    return this.usersService.createUser({ nickname, email, password });
   }
 }
