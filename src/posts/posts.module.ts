07ffb012106c581ce7611b23afffc5ded2c0a4f3
KSY <sye.g112@gmail.com>
2025-08-05 11:16:52 +0900

    fix: 의존성 오류 해결

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,12 +3,14 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 // TypeOrmModule.forRoot()는 typeORM에 연결 설정을 할 때, appmodule에서 사용하고
 // Feature는 모델에 해당되는 레포지토리를 주입할 때 사용한다.
 // PostsModel를 넣으면 typeORM이 알아서 model과 관련된 레포지토리를 만들어 준다.
 @Module({
-  imports: [TypeOrmModule.forFeature([PostsModel])],
+  imports: [TypeOrmModule.forFeature([PostsModel]), AuthModule, UsersModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
