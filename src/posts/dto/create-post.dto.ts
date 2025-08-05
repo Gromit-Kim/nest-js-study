e2e86b6f779fb8a0bdb55a9c5078def55d340c08
KSY <sye.g112@gmail.com>
2025-08-05 13:18:43 +0900

    refactor(createpostdto): picktype을 이용해 효율적으로 class-validator사용

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,13 +1,7 @@
-import { IsString } from 'class-validator';
+import { PickType } from '@nestjs/mapped-types';
+import { PostsModel } from '../entities/posts.entity';
 
-export class CreatePostDto {
-  @IsString({
-    message: 'title은 string 타입을 입력해주어야 합니다.',
-  })
-  title: string;
+// Pick, Omit, Partial - ts -> 타입을 반환
+// PickType, OmitType, PartialType -> 값을 반환 (extends를 할 수 있다.)
 
-  @IsString({
-    message: 'content는 string 타입을 입력해주어야 합니다.',
-  })
-  content: string;
-}
+export class CreatePostDto extends PickType(PostsModel, ['title', 'content']) {}
--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,3 +1,4 @@
+import { IsString } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { UsersModel } from 'src/users/entities/users.entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
@@ -21,9 +22,15 @@ export class PostsModel extends BaseModel {
   author: UsersModel;
 
   @Column()
+  @IsString({
+    message: 'title은 string 타입을 입력해주어야 합니다.',
+  })
   title: string;
 
   @Column()
+  @IsString({
+    message: 'content는 string 타입을 입력해주어야 합니다.',
+  })
   content: string;
 
   @Column()
