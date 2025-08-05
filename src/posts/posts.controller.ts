483c73a4b8d046a686d485ffaa5cce391c5801a7
KSY <sye.g112@gmail.com>
2025-08-05 14:21:03 +0900

    feat: posts에 딸린 사용자 정보에도 pw가 보여져선 안된다.

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,6 +10,8 @@ import {
   DefaultValuePipe,
   UseGuards,
   Request,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
@@ -25,6 +27,7 @@ export class PostsController {
   // 1) GET /posts
   //    모든 post를 다 가져온다.
   @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
   getPosts() {
     return this.postsService.getAllPosts();
   }
