27afd38d4099039dd99ddb9e296e6e9c8ca1255d
KSY <sye.g112@gmail.com>
2025-08-05 13:13:08 +0900

    feat: class-validator에 custom message 적용

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,7 +1,9 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력해주어야 합니다.',
+  })
   title: string;
 
   @IsString()
