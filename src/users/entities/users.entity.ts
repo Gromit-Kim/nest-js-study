c236d51a8cce1595bbddc09222a20963c0852e20
KSY <sye.g112@gmail.com>
2025-08-05 13:59:09 +0900

    refactor: validation-message 일반화

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -8,6 +8,7 @@ import {
   Length,
   ValidationArguments,
 } from 'class-validator';
+import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -17,23 +18,7 @@ export class UsersModel extends BaseModel {
   })
   @IsString() // 제약 사항을 직접 넣어주지 않는 에노테이션은 어떤 값을 넣었는지 순서대로 받아볼 수 없다.
   @Length(1, 20, {
-    message(args: ValidationArguments) {
-      /**
-       * ValidationArguments의 프로퍼티들
-       *
-       * 1) value -> 검증 되고 있는 값 (입력된 값)
-       * 2) constraints -> 파라미터에 입력된 제한 사항들
-       *    args.constraints[0] - 1
-       *    args.constraints[1] - 20
-       * 3) targetName -> 검증하고 있는 클래스의 이름
-       * 4) object -> 검증하고 있는 객체
-       * 5) property -> 검증되고 있는 객체의 프로퍼티 이름, 예) nickname
-       */
-      if (args.constraints.length === 2) {
-        return `${args.property}은 ${args.constraints[0]}~${args.constraints[1]} 글자를 입력해주세요.`;
-      }
-      return `${args.property}은 최새 ${args.constraints[0]}글자를 입력해주세요.`;
-    },
+    message: lengthValidationMessage,
   })
   nickname: string;
 
@@ -46,7 +31,9 @@ export class UsersModel extends BaseModel {
 
   @Column()
   @IsString()
-  @Length(3, 8)
+  @Length(3, 8, {
+    message: lengthValidationMessage,
+  })
   password: string;
 
   @Column({
