4220a81f4201baca900345d6550ee4b4d9519bd8
KSY <sye.g112@gmail.com>
2025-08-06 17:36:14 +0900

    feat: 내림차순 정렬을 위한 less than id 추가

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -2,6 +2,10 @@ import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class PaginatePostDto {
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?: number; // DESC를 위해서 필요하다.
+
   // 이전 마지막 데이터의 ID
   // 이 프로퍼티에 입력된 ID 보도 높은 ID 부터 값을 가져오기
   //   @Type(() => Number) // transformer 기능
@@ -13,7 +17,7 @@ export class PaginatePostDto {
   // createdAt -> 생성된 시간의 내림차/오름차 순으로 정렬
   @IsIn(['ASC', 'DESC'])
   @IsOptional()
-  order__createdAt: 'ASC' = 'ASC';
+  order__createdAt: 'ASC' | 'DESC' = 'ASC';
 
   // 몇 개의 데이터를 응답으로 받을지
   @IsNumber()
