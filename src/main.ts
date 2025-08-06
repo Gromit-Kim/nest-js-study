733674b1e3c0c4cd72089f1e54ea40d5bbebacc3
KSY <sye.g112@gmail.com>
2025-08-06 13:54:24 +0900

    fix: class-validator와 class-transformer는 사용자가 보낸 값을 그대로 적용한다. 그 값을 넣지 않더라도 default value를 dto에 적용하도록(변형하도록) 옵션을 넣는다.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 값을 넣지 않더라도 default value로 dto를 형성하게 하기 위함.
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
